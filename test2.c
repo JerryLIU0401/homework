609966fc9f3574f4fc649430aa4eb518e65bf782
JerryLIU0401 <91183020+JerryLIU0401@users.noreply.github.com>
2021-09-24 15:52:08 +0800

    Add files via upload

---
+++ test2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main ( void ){ 
+    printf("%s\n", "My neame is 劉哲睿");
+    printf("%s\n", "My ID is D1051786");
+    printf("%s\n", "My major is 資訊工程");    
+    return 0;
+} 
